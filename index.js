af6943a2c4751913023913fb7b5bc58bbba1d841
3y3 <3y3@ya.ru>
2024-08-08 11:49:41 +0300

    fix: Import order

--- index.js
+++ index.js
@@ -16,5 +16,24 @@ module.exports = {
         'no-implicit-globals': 'off',
         'no-param-reassign': 'off',
         '@typescript-eslint/no-shadow': 'off',
+        'import/no-extraneous-dependencies': 'error',
+        'import/order': [
+            'error',
+            {
+                alphabetize: {
+                    order: 'ignore',
+                    orderImportKind: 'asc'
+                },
+                'newlines-between': 'always',
+                groups: ['type', ['builtin', 'external'], 'internal', 'parent', 'sibling', 'index'],
+                warnOnUnassignedImports: true,
+                pathGroups: [
+                    {
+                        pattern: '*.s?css$',
+                        group: 'index',
+                    },
+                ],
+            },
+        ],
     },
 };
